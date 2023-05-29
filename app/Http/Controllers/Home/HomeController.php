2bbcd62c1cc364622092b84eff2022316cf97f63
Kanchan <kanchan@atinatechnology.in>
2023-05-29 18:54:09 +0530

    Changes in controller

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -18,8 +18,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $categories = '';
-        // Category::all();
+        $categories = Category::all();
         return view('home', compact('categories'));
     }
 
